0801c0de8f57366b60bd2e45bc57969d0c94328f
Maria <m.muszynska098@gmail.com>
2024-11-27 09:37:05 +0100

    user controller update

--- controllers/user.py
+++ controllers/user.py
@@ -3,7 +3,17 @@ from models.receipt import Receipt
 
 
 class UserController:
-    def
+    def create(self, email, username, password, full_name=""):
+        new_user = User(email=email, username=username, password=password, full_name=full_name)
+        new_user.save()
+    
+    def delete(self, email):
+        user = User.objects(email=email).first()
+        # delete user receipts, not allow dangling leftovers
+        for receipt in user.receipts:
+            receipt.delete()
+        user.delete()
+
     def link_receipt(self, user_email: str, receipt: Receipt):
         user = User.objects(email=user_email).first()
         if not user:
